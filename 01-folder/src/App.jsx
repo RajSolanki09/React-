698d8bf6da4d371bfa12cf3c745431bf870b1bdc
3811 Raj Solanki <solankiraj9642@gmail.com>
2026-01-08 10:27:14 +0530

    01-folder done

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <div id="Dad">
+        <h1 id="child1">hello</h1>
+        <h1 id="child2">hello2</h1>
+      </div>
+      <div id="chacha"></div>
+    </>
+  );
+};
+
+export default App;
